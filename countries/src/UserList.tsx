a89daa301c40386d7f2bb88a3502712f9ebd821c
Alex Melvin <alex.melvin99.com>
2021-08-16 16:14:55 -0400

    created UserList component

---
+++ countries/src/UserList.tsx
@@ -0,0 +1,31 @@
+import React, {FunctionComponent} from 'react';
+import './App.css';
+import {
+  UnorderedList,
+  ListItem,
+  Text,
+} from "@chakra-ui/react"
+import _ from "lodash"
+import {useGenderContext} from "./GenderContext"
+
+    import {CountryUser} from './types'
+
+    type UserListProps = {
+        users : Array<CountryUser>
+    }
+
+    export const UserList: FunctionComponent<UserListProps> = ({users}) => {
+        const {gender} = useGenderContext()
+        return (
+            <UnorderedList>
+                {users
+                    .filter((user) => gender === "all" || gender === user["gender"])
+                    .map((user) => (
+                        <ListItem as={Text} fontSize="sm" key={user["name"]}>
+                            {`${user["name"]}, ${user["gender"]}, ${user["city"]}, ${user["state"]}, ${user["registered"].toDateString()}`}
+                        </ListItem>
+                    )
+                )}
+            </UnorderedList>
+        )
+    }
